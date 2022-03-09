910777915552bceaa6d591ad815d7bf74576153d
DrYury <yury.danilyuk@gmail.com>
2022-03-10 03:53:27 +0700

    Improved implementation of custom aspect

--- server/src/main/java/com/nexign/bootcamp/aop/spring/server/Controller.java
+++ server/src/main/java/com/nexign/bootcamp/aop/spring/server/Controller.java
@@ -2,14 +2,20 @@ package com.nexign.bootcamp.aop.spring.server;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 public class Controller {
     @MeteredDoSomething
     @PostMapping("/doSomething")
-    public Response doSomething(@RequestBody Request request) {
+    public Response doSomething(@RequestBody Request request,
+                                @RequestPart(required = false) Optional<AtomicReference<String>> metricSection) {
         String user = (request.getUser() != null) ? request.getUser() : "Somebody";
+        metricSection.ifPresentOrElse(s -> {s.set(user);}, () -> {});
         long millis = Worker.imitate(0.5);
         return new Response("Semi-computational result for " + user, millis);
     }
--- server/src/main/java/com/nexign/bootcamp/aop/spring/server/MeteredDoSomethingAspect.java
+++ server/src/main/java/com/nexign/bootcamp/aop/spring/server/MeteredDoSomethingAspect.java
@@ -1,24 +1,70 @@
 package com.nexign.bootcamp.aop.spring.server;
 
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class MeteredDoSomethingAspect {
+    private static final String SECTION_PARAM_NAME = "metricSection";
+    private static final String SECTION_PARAM_TYPE_NAME =
+            "java.util.Optional<java.util.concurrent.atomic.AtomicReference<java.lang.String>>";
+
     private final PrometheusMeterRegistry registry;
+    private final ConcurrentHashMap<String, Double> lastExecutionTime = new ConcurrentHashMap<>();
 
     @Around("@annotation(com.nexign.bootcamp.aop.spring.server.MeteredDoSomething)")
     public Object meterRestMethod(ProceedingJoinPoint pjp) throws Throwable {
+        AtomicReference<String> user = null;
+        Object[] args = null;
         try {
-            return pjp.proceed();
+            Method method = ((MethodSignature) pjp.getSignature()).getMethod();
+            Parameter[] params = method.getParameters();
+            if (params != null && params.length > 0) {
+                String paramName = params[params.length - 1].getName();
+                String paramTypeName = params[params.length - 1].getParameterizedType().getTypeName();
+                if (SECTION_PARAM_NAME.equals(paramName) && SECTION_PARAM_TYPE_NAME.equals(paramTypeName)) {
+                    user = new AtomicReference<>();
+                    Object[] prevArgs = pjp.getArgs();
+                    args = Arrays.copyOf(prevArgs, prevArgs.length);
+                    args[args.length - 1] = Optional.of(user);
+                }
+            }
+        } catch (Throwable ignore) {
+        }
+        StopWatch sw = new StopWatch();
+        sw.start();
+        try {
+            if (args == null) {
+                return pjp.proceed();
+            } else {
+                return pjp.proceed(args);
+            }
         } finally {
-            registry.counter("doSomething").increment();
+            sw.stop();
+            registry.counter("doSomething_all").increment();
+            if (user != null) {
+                Tags tags = Tags.of("user", user.get());
+                registry.counter("doSomething", tags).increment();
+                final String userName = user.get();
+                lastExecutionTime.put(userName, sw.getTotalTimeSeconds());
+                registry.gauge("doSomething_last", tags, lastExecutionTime, l -> l.get(userName));
+            }
         }
     }
 }
